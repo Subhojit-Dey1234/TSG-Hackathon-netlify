8b6a7787c20374898fdf5789ead3fd7ae2a8321b
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-01-11 22:33:01 +0530

    Changes made by Tarafdar

--- client/src/Components/News/index.js
+++ client/src/Components/News/index.js
@@ -9,28 +9,45 @@ import {
 	Button,
 	Modal,
 	ModalBody,
-  Form,
-  FormGroup,
-  Input
+	Form,
+	FormGroup,
+	Input,
 } from "reactstrap";
 import logo1 from "../../Images/logo1.png";
 import logo2 from "../../Images/logo2.png";
 import logo4 from "../../Images/logo4.png";
+import formImage from '../../Images/FormImage.png'
 import { Row, Col } from "reactstrap";
 const Example = (props) => {
-  const [ uploadNews, setUploadNews ] = useState(false)
+	const [uploadNews, setUploadNews] = useState(false);
 	return (
 		<div>
 			<br />
 			<br />
 			<br />
-			<h2 style={{ textAlign: "left", margin: "3% 5%", fontWeight: "bolder" }}>
+			<h2 style={{ textAlign: "left", margin: "3% 5% 0 5%", fontWeight: "bolder" }}>
 				Get Latest News!
 			</h2>
-			<Button onClick={()=>{
-        setUploadNews(true)
-      }}>Upload</Button>
-			<Modal centered isOpen={uploadNews} toggle={()=>setUploadNews(false)} >
+			{/* <Button
+				onClick={() => {
+					setUploadNews(true);
+				}}
+			>
+				Upload
+			</Button> */}
+			<a
+						href={() => false}
+						onClick={()=>{setUploadNews(true)}}
+						style={{
+							textDecoration: "none",
+							margin: "3% 5% 0 5%",
+							color: "blue",
+							cursor: "pointer",
+						}}
+					>
+						Want to Create A News Now?
+					</a>
+			<Modal centered isOpen={uploadNews} toggle={() => setUploadNews(false)}>
 				<ModalBody>
 					<Row
 						style={{
@@ -39,9 +56,11 @@ const Example = (props) => {
 							padding: "0 5%",
 						}}
 					>
-						<h2 style={{ textAlign: "left", fontWeight: "bolder" }}>
+						<h4 style={{ textAlign: "center" }}>
 							Upload The News
-						</h2>
+						</h4>
+						<br/>
+						<img src={formImage} />
 						<Col
 							sm="12"
 							style={{ alignItems: "center", justifyContent: "center" }}
@@ -50,39 +69,23 @@ const Example = (props) => {
 								<FormGroup>
 									<Input
 										required={true}
-										name="name"
-										placeholder="Name of The Event"
+										name="title"
+										placeholder="Enter The Title"
 										type="name"
+										value={"user.title"}
+										onChange={(e)=>{}}
 									/>
 								</FormGroup>
 								<Row>
-									<Col sm="6">
-										<FormGroup>
-											<Input
-												required={true}
-												name="date"
-												placeholder="Schedule of The Event"
-												type="datetime-local"
-											/>
-										</FormGroup>
-									</Col>
-									<Col sm="6">
-										<FormGroup>
-											<Input
-												required={true}
-												name="date"
-												placeholder="Schedule of The Event"
-												type="datetime-local"
-											/>
-										</FormGroup>
-									</Col>
 									<Col>
 										<FormGroup>
 											<Input
 												required={true}
-												name="organizer"
-												placeholder="Enter Organizing Body"
+												name="name"
+												placeholder="Enter Your Name or Organization"
 												type="name"
+												value={"user.name"}
+												onChange={(e)=>{}}
 											/>
 										</FormGroup>
 									</Col>
@@ -91,8 +94,10 @@ const Example = (props) => {
 									<Input
 										required={true}
 										name="description"
-										placeholder="Enter Event Description"
+										placeholder="Enter News Description"
 										type="textarea"
+										value={"user.news"}
+										onChange={(e)=>{}}
 									/>
 								</FormGroup>
 								<FormGroup>
@@ -151,48 +156,7 @@ const Example = (props) => {
 					</Col>
 				</Row>
 			</Card>
-			<Card
-				style={{ textAlign: "left", boxShadow: "2px grey", margin: "3% 5%" }}
-			>
-				<Row>
-					<Col sm="3">
-						<CardImg top width="auto" src={logo4} alt="Card image cap" />
-					</Col>
-					<Col sm="9">
-						<CardBody>
-							<CardTitle tag="h5">Project title</CardTitle>
-							<p>Prof. Richard Feyman | Department of Physics</p>
-							<CardText>
-								Lorem Ipsum is simply dummy text of the printing and typesetting
-								industry. Lorem Ipsum has been the industry's standard dummy
-								text ever since the 1500s, when an unknown printer took a galley
-								of type and scrambled it to make a type specimen book.
-							</CardText>
-						</CardBody>
-					</Col>
-				</Row>
-			</Card>
-			<Card
-				style={{ textAlign: "left", boxShadow: "2px grey", margin: "3% 5%" }}
-			>
-				<Row>
-					<Col sm="3">
-						<CardImg top width="auto" src={logo4} alt="Card image cap" />
-					</Col>
-					<Col sm="9">
-						<CardBody>
-							<CardTitle tag="h5">Project title</CardTitle>
-							<p>Prof. Richard Feyman | Department of Physics</p>
-							<CardText>
-								Lorem Ipsum is simply dummy text of the printing and typesetting
-								industry. Lorem Ipsum has been the industry's standard dummy
-								text ever since the 1500s, when an unknown printer took a galley
-								of type and scrambled it to make a type specimen book.
-							</CardText>
-						</CardBody>
-					</Col>
-				</Row>
-			</Card>
+			
 			<br />
 			<div style={{ padding: "3% 5%" }}>
 				<h2 style={{ textAlign: "left", fontWeight: "bolder" }}>
