8d0b83b45c3bba23c7dd6e6a6046f7233dfb2d63
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-01-17 21:06:29 +0530

    Notification UI

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ import { Alert, Button, Modal, ModalBody } from "reactstrap";
 import { useEffect, useState } from "react";
 import Notification from "./Components/Notifications/Notification";
 
-var socket = io("http://localhost:5000/");
+var socket = io("https://hackathon-tsg.herokuapp.com/");
 function App() {
 	const [notification, setNotification] = useState(null);
 	const [openNotify, setNotify] = useState(false);
