f2648a07edc7e97be1c0a1c1a5956a0c7834a59f
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-01-23 11:34:38 +0530

    Changed

--- client/src/Components/Login/StudentLogin.js
+++ client/src/Components/Login/StudentLogin.js
@@ -36,9 +36,7 @@ export default function StudentLogin() {
 					if (res.status === 200) {
 						setLoad(false);
 						setMsg(true);
-						setInterval(() => {
-							window.location.href = "/otp";
-						}, 200);
+						window.location.href = "/otp"
 					}
 					let interval;
 					if (res.status === 400) {
